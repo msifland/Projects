350ac38851c62f9159897e75f9d8b52e83b90f7c
msifland <msifland@gmail.com>
2025-11-20 19:25:56 -0600

    auto commit

---
+++ Python/mory-py.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+print('for loop')
+for i in range(1,10):
+	print(i)
+print()
+
+print('while loop')
+a = 1
+while a <10:
+	print (a)
+	a = a + 1 # Or a+=1
+print()
+
+print('Python Lists')
+myList = [1,2,3,4,5,6,7,8]
+print('Single number')
+print(myList[3])
+print('Whole list using loop')
+for i in myList:
+	print(i)
+
+import subprocess
+print()
+print('my test list')
+
+
