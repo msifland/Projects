6ca67ea973ab7b0e1a8c1acd5777dd3a40c0655e
msifland <msifland@gmail.com>
2025-11-29 17:51:44 -0600

    auto commit

---
+++ Python/grok-py.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# This is a blank python script document.
+# Put your code below this line.
+print ()
+print ("This is a python script")
+print ()
+
+name = input ("What is your name? ")
+print ()
+print (".....AND how old are you? ")
+age = input ("DON'T LIE EITHER!!! ")
+print ()
+
+print (f"Wow {name}, you are that old!!! {age}!!!")
+print ()
+print (f"That means, next year you'll be {int(age)+1}")
+print ()
+print (f"Damn {name}....you an old bastard at {age}!!!")
+print ()
+print (f"Python says: {name}, you should be dead by now! Shit, at age {age}!!")
+print ()
+
